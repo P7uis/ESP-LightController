f11d5267c16e0b1880023e9018e17111fec6c584
Wesley Eenjes <P7uisdev@gmail.com>
2021-01-16 23:59:45 +0100

    New startup tune

--- HandyDandyInfo/test_new_tune/test_new_tune.ino
+++ HandyDandyInfo/test_new_tune/test_new_tune.ino
@@ -1,9 +1,17 @@
+#include <Tone32.h>
+
+// Piezo buzzer pin & channel
+int BUZZER_PIN = 16;
+int BUZZER_CHANNEL = 0;
+
+
 #define N_D4  294
 #define N_F4  349
 #define N_G4  392
 #define N_GS4 415
 #define N_A4  440
 #define N_D5  587
+
 
 int const TEMPO = 1200;
 
@@ -13,9 +21,9 @@ int melody[] = {
 int noteDurations[] = {
   16, 16, 8, 6, 32, 8, 8, 8, 16, 16, 16};
 
-void ESPLC_Buzzer(int BuzzerTune){
-  // Startup tune
-  if(BuzzerTune == 0){
+//melody = 266 notes
+
+void setup() {
   int melody_len = sizeof(melody)/sizeof(melody[0]);
 
   for (int thisNote = 0; thisNote < melody_len; thisNote++) {
@@ -26,22 +34,6 @@ void ESPLC_Buzzer(int BuzzerTune){
     noTone(BUZZER_PIN, BUZZER_CHANNEL);
     tone(BUZZER_PIN, NOTE_B6, 100, BUZZER_CHANNEL);
   }
-  }
-  // Button click sound
-  else if(BuzzerTune == 1){
-        tone(BUZZER_PIN, NOTE_E6, 100, BUZZER_CHANNEL);
-        noTone(BUZZER_PIN, BUZZER_CHANNEL);
-        tone(BUZZER_PIN, NOTE_F6, 100, BUZZER_CHANNEL);
-        noTone(BUZZER_PIN, BUZZER_CHANNEL);
-  }
-  // Button scroll right sound
-  else if(BuzzerTune == 2){
-        tone(BUZZER_PIN, NOTE_D4, 100, BUZZER_CHANNEL);
-        noTone(BUZZER_PIN, BUZZER_CHANNEL);
-  }
-  // Button scroll leftsound
-  else if(BuzzerTune == 3){
-        tone(BUZZER_PIN, NOTE_C4, 100, BUZZER_CHANNEL);
-        noTone(BUZZER_PIN, BUZZER_CHANNEL);
-  }
 }
+
+void loop() {}
