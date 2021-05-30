7058791b6ce30f769876983556f1676f3896115a
Wesley Eenjes <P7uisdev@gmail.com>
2021-05-30 17:01:11 +0200

    Add pinout and removed some obsolete files

--- HandyDandyInfo/CopyPasteTemplate.ino
+++
@@ -1,6 +0,0 @@
-// Check if in roof light OI menu
-      if(MenuIndex == 13){
-        // Reset to last if already on the first one
-        if(RLOIMenuPosition == 0) RLOIMenuPosition = RLOIMenuLength;
-        else RLOIMenuPosition--; 
-      }
